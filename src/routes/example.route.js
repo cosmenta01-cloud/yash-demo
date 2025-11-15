41791bfadeeab10ec2bc39c527bdc3ff2dae463e
cosmenta01-cloud <cosmenta01@gmail.com>
2025-11-15 19:21:56 +0530

    Add src/routes/example.route.js

---
+++ src/routes/example.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getAll,
+  getById,
+  create
+} = require('../controllers/example.controller');
+
+router.get('/', getAll);
+router.get('/:id', getById);
+router.post('/', create);
+
+module.exports = router;
